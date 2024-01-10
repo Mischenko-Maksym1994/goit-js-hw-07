7a4794af597d5bc81310d192f85230222582f82c
Mischenko-Maksym1994 <MischenkoMaksym1994@gmail.com>
2024-01-10 21:03:02 +0200

    add style for task-3

---
+++ js/task-3.js
@@ -0,0 +1,16 @@
+
+const nameInput = document.querySelector("#name-input");
+console.log(nameInput);
+
+const nameOutput = document.querySelector("#name-output");
+console.log(nameOutput);
+
+nameInput.addEventListener("input", (event) => {nameOutput.textContent = event.currentTarget.value;
+});
+
+
+const classbox = document.querySelector("body div");
+classbox.classList.add("box");
+
+const textStyle = document.querySelector("body h1");
+textStyle.classList.add("text-style");
