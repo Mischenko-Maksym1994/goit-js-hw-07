c160d4eb16482b80e8d9bd9faec019674226623a
Mischenko-Maksym1994 <MischenkoMaksym1994@gmail.com>
2024-01-16 21:03:02 +0200

    add JS and CSS for task-6

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,45 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnCreate = document.querySelector("[data-create]");
+const btnDestroy = document.querySelector("[data-destroy]");
+const input = document.querySelector("input");
+const divBoxes = document.querySelector("#boxes");
+
+btnCreate.addEventListener("click", () => {
+  const amount = input.value;
+  if (amount >= 1 && amount <= 100) {
+    input.value = "";
+    createBoxes(amount);
+  }});
+
+btnDestroy.addEventListener("click", destroyBoxes);
+
+
+function createBoxes(amount) {
+  // destroyBoxes();
+
+  let width = 30;
+  let height = 30;
+  const step = 10;
+  const arrayElement = [];
+
+  for (let i = 0; i < amount; i += 1) {
+    const newElement = document.createElement("div");
+    newElement.style.height = `${height}px`;
+    newElement.style.width = `${width}px`;
+    newElement.style.backgroundColor = getRandomHexColor();
+
+    arrayElement.push(newElement);
+    width += step;
+    height += step;
+  }
+  
+  divBoxes.append(...arrayElement);
+  console.log(`create ${amount} elements`);
+}
+
+function destroyBoxes() {
+  divBoxes.innerHTML = "";
+}
