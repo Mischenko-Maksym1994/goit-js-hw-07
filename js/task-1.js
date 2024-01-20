9967e7b506033d4c851db3d97e34c3cea55fea41
Mischenko-Maksym1994 <MischenkoMaksym1994@gmail.com>
2024-01-20 21:05:48 +0200

    add fixed cod for task-1

--- js/task-1.js
+++ js/task-1.js
@@ -2,18 +2,19 @@
 const category = document.querySelector("#categories");
 
 const arrayItem = [...category.children];
+const lengthArray = arrayItem.length;
+console.log(`Number of categories: ${lengthArray}`);
 
-function titleCategory(array) {
+function logCategoryDetails(array) {
     for (let element of array) {
         let firstTitle = element.firstElementChild;
         let lastChild = element.lastElementChild;
         let lengthLastChild = lastChild.children.length;
-        // console.log(lengthLastChild);
-        // console.log(lastChild);
+        
         console.log(`Category: ${firstTitle.textContent}`);
         console.log(`Elements: ${lengthLastChild}`);
      }
 };
 
-console.log(titleCategory(arrayItem));
+logCategoryDetails(arrayItem);
 
